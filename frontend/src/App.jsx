be15a8b670b7b54c081bbbe31dca425a1987a50a
Ajilalitha <ajilalitha010@gmail.com>
2024-12-24 13:40:35 +0530

    add the color, fonts family

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <nav>Navbar</nav>
-       <main className="min-h-screen">
+       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
        <Outlet/> 
        </main>     {/*outlet used to the rendered the children */}
       <footer>Footer</footer>
