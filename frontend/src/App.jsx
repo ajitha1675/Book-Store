81902c54a44d551cf1a8d762e8edafb2b27a1993
Ajilalitha <ajilalitha010@gmail.com>
2024-12-25 14:14:54 +0530

    added the footer page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   
@@ -10,7 +11,7 @@ function App() {
        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
        <Outlet/> 
        </main>     {/*outlet used to the rendered the children */}
-      <footer>Footer</footer>
+       <Footer/>
     </>
   )
 }
