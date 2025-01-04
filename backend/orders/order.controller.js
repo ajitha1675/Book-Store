767ea5293755e96ca8c3e02e6c766c396f038450
Ajilalitha <ajilalitha010@gmail.com>
2025-01-04 14:24:34 +0530

    creating orderpage

--- backend/orders/order.controller.js
+++ backend/orders/order.controller.js
@@ -13,6 +13,25 @@ const createAOrder = async(req, res) =>{
   }
 };
 
+const getOrderByEmail = async(req,res)=>{
+  try { 
+    const {} = req.params;
+    const orders = await Order.find({email}).sort({createdAt: -1});
+    if(!orders) {
+      return res.status(404).json({
+        message: "Order not found"
+      })
+    }
+     
+  } catch (error) {
+     console.error("Error fetching orders ", error);
+     res.status(500).json({
+      message: "Failed to fetch order"
+     })
+  }
+}
+
 module.exports = {
   createAOrder,
+  getOrderByEmail
 }
