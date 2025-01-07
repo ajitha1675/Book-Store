321d6abe741dd7ee7284128f537b9ab0fd867ab9
Ajilalitha <ajilalitha010@gmail.com>
2025-01-07 19:37:56 +0530

    creates the userdashboard

--- frontend/src/routers/Router.jsx
+++ frontend/src/routers/Router.jsx
@@ -15,6 +15,7 @@ import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
 import AddBook from "../pages/dashboard/addBook/AddBook";
 import UpdateBook from "../pages/dashboard/editbook/UpdateBook";
+import UserDashboard from "../pages/dashboard/users/UserDashboard";
 
 const router = createBrowserRouter([
         {
@@ -52,6 +53,10 @@ const router = createBrowserRouter([
             {
               path: "/books/:id",
               element: <SingleBook/>
+            },
+            {
+              path: "/user-dashboard",
+              element: <PrivateRoute><UserDashboard/></PrivateRoute>
             }
           ]
         },
